95952cad2d33d4dde9bcfbcbb1f6e7dad2c4edfd
gainhad <hadley.gaines@gmail.com>
2019-06-26 16:17:06 -0400

    cleaned up eslint warnings

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
--- src/components/NewSet.js
+++ src/components/NewSet.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './NewSet.scss';
 
 const NewSet = props => {
@@ -34,7 +34,6 @@ const NewSet = props => {
       />
       <input
         type="button"
-        value="2"
         value="CANCEL"
         onClick={props.toggleModal}
       />
