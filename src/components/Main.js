4e6cf9e00e299c39758639c06698fab7bac15f86
gainhad <hadley.gaines@gmail.com>
2019-07-09 09:40:21 -0400

    moved blur/darken modal background to redux state

---
+++ src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
+import Workout from './Workout';
+import { useSelector } from 'react-redux';
+
+function Main() {
+  const isBlurred = useSelector(state => state.globalUI.isBlurred);
+  return (
+    <div className={isBlurred ? "blurred" : null} id="App">
+      <Router>
+        <Route path="/" exact component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/workout" component={Workout} />
+      </Router>
+    </div>
+  );
+}
+
+export default Main;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import { useDispatch } from 'react-redux';
+import { globalUIActions } from '../redux/slices/globalUI';
 
 import './Modal.scss';
 
 const Modal = props => {
   const [isOpen, setIsOpen] = useState(props.isOpen);
+  const dispatch = useDispatch();
   const closeModal = () => {
     setIsOpen(false);
     props.registerClose();
+    dispatch(globalUIActions.isBlurred(false));
   };
-  useEffect(() => setIsOpen(props.isOpen), [props.isOpen]);
+
+  // Update isOpen when props.isOpen changes.
+  useEffect(() =>{
+    setIsOpen(props.isOpen);
+    if (props.isOpen) {
+      dispatch(globalUIActions.isBlurred(true));
+    }
+  }, [props.isOpen]);
 
   if (isOpen) {
     return ReactDOM.createPortal(
