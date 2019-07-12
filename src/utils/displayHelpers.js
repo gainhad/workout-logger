603a3562dea8312d80c29ae11cd85d0326baa96e
gainhad <hadley.gaines@gmail.com>
2019-07-12 14:43:30 -0400

    moved all frontend files to client folder

--- src/utils/displayHelpers.js
+++
@@ -1,9 +0,0 @@
-function prettyDisplayTime(seconds) {
-  let displayMinutes = Math.floor(seconds / 60);
-  let displaySeconds = Math.floor(seconds % 60);
-  return displaySeconds < 10
-    ? `${displayMinutes}:0${displaySeconds}`
-    : `${displayMinutes}:${displaySeconds}`;
-}
-
-export { prettyDisplayTime };
