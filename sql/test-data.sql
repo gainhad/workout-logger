ec041aefc13f20b660cb7e752ce750b13850dd92
gainhad <hadley.gaines@gmail.com>
2019-07-15 12:14:52 -0400

    Updated test database data

--- sql/test-data.sql
+++ sql/test-data.sql
@@ -29,26 +29,26 @@ INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('de
 INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 3, to_timestamp(1562953528), 145, 5, 8);
 
 --- sets for workout 2;
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 4, to_timestamp(1562865328), 125, 5, 6);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 4, to_timestamp(1562865328), 135, 5, 7);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 4, to_timestamp(1562865328), 145, 5, 8);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 5, to_timestamp(1562866228), 125, 5, 6);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 5, to_timestamp(1562866228), 135, 5, 7);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 5, to_timestamp(1562866228), 145, 5, 8);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 6, to_timestamp(1562867128), 125, 5, 6);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 6, to_timestamp(1562867128), 135, 5, 7);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 6, to_timestamp(1562867128), 145, 5, 8);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 4, to_timestamp(1563059325), 125, 5, 6);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 4, to_timestamp(1563059325), 135, 5, 7);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 4, to_timestamp(1563059325), 145, 5, 8);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 5, to_timestamp(1563059325), 125, 5, 6);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 5, to_timestamp(1563059325), 135, 5, 7);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 5, to_timestamp(1563059325), 145, 5, 8);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 6, to_timestamp(1563059325), 125, 5, 6);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 6, to_timestamp(1563059325), 135, 5, 7);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 6, to_timestamp(1563059325), 145, 5, 8);
 
 --- sets for workout 3;
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 7, to_timestamp(1562778928), 125, 5, 6);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 7, to_timestamp(1562778928), 135, 5, 7);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 7, to_timestamp(1562778928), 145, 5, 8);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 8, to_timestamp(1562779828), 125, 5, 6);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 8, to_timestamp(1562779828), 135, 5, 7);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 8, to_timestamp(1562779828), 145, 5, 8);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 9, to_timestamp(1562780728), 125, 5, 6);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 9, to_timestamp(1562780728), 135, 5, 7);
-INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 9, to_timestamp(1562780728), 145, 5, 8);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 7, to_timestamp(1563145725), 125, 5, 6);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 7, to_timestamp(1563145725), 135, 5, 7);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 7, to_timestamp(1563145725), 145, 5, 8);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 8, to_timestamp(1563145725), 125, 5, 6);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 8, to_timestamp(1563145725), 135, 5, 7);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 8, to_timestamp(1563145725), 145, 5, 8);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 9, to_timestamp(1563145725), 125, 5, 6);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 9, to_timestamp(1563145725), 135, 5, 7);
+INSERT INTO sets(username, liftID, time_completed, weight, reps, rpe) VALUES('demoUser', 9, to_timestamp(1563145725), 145, 5, 8);
 
 
 -- Test data for measurement history
