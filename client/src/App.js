41d323df04ec5987554f6c093c452ebd8747a408
gainhad <hadley.gaines@gmail.com>
2019-08-05 09:39:59 -0400

    Updated styles nav button to work better on mobile

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Main from "./Main";
 import store from "./redux/store.js";
 import { Provider } from "react-redux";
 import "./style/colors.css";
+import "./style/variables.css";
 
 function App() {
   return (
