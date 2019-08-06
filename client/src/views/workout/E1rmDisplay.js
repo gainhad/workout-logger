763860522d0da40b5649c5fbb9eb639ad9f5aee3
gainhad <hadley.gaines@gmail.com>
2019-08-06 10:46:32 -0400

    Updated E1rm to react-redux hooks and css modules

--- client/src/views/workout/E1rmDisplay.js
+++ client/src/views/workout/E1rmDisplay.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./E1rmDisplay.scss";
-import { connect } from "react-redux";
+import styles from "./E1rmDisplay.module.scss";
+import { useSelector } from "react-redux";
 import { getEstimatedOneRepMax } from "../../redux/slices/currentWorkout";
 
 const E1rmDisplay = props => {
-  const weight = Math.round(props.estimatedOneRepMax);
+  const estimatedOneRepMax = useSelector(state => getEstimatedOneRepMax(state));
+  const weight = Math.round(estimatedOneRepMax);
   if (weight) {
     return (
-      <div className="e1rm-display">
+      <div className={styles.e1rmDisplay}>
         <b>E1RM:</b> {weight} lbs
       </div>
     );
@@ -16,13 +17,4 @@ const E1rmDisplay = props => {
   }
 };
 
-const mapStateToProps = state => {
-  return {
-    estimatedOneRepMax: getEstimatedOneRepMax(state)
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  null
-)(E1rmDisplay);
+export default E1rmDisplay;
